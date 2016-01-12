36fed172a9cb2e72456bad3e82f9c35023dcc3c6
Alex Ryan <ialex.ryan@gmail.com>
2016-01-12 10:31:12 -0800

    Prompt with current directory

--- src/shell/mysh.c
+++ src/shell/mysh.c
@@ -1,12 +1,21 @@
+// apt-get install libreadline-dev libbsd-dev
+
 #include <stdlib.h>
 #include <stdbool.h>
+#include <bsd/string.h>
+#include <unistd.h>
 #include <readline/readline.h>
 #include <readline/history.h>
 
 int main() {
     char* line;
+    char prompt[80];
     while(true) {
-        line = readline("Enter a string: ");
+        char cwd_buf[80];
+        getcwd(cwd_buf, sizeof(cwd_buf));
+        strlcpy(prompt, cwd_buf, sizeof(prompt));
+        strlcat(prompt, " ", sizeof(prompt));
+        line = readline(prompt);
         add_history(line);
         printf("%s\n", line);
         free(line);
