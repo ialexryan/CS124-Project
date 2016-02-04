7986835eee1ed12150336edc12f1b74dff47131a
Alex Ryan <alex@alexryan.org>
2016-02-03 20:38:37 -0800

    Add prototype for priority_less_func

--- src/threads/thread.c
+++ src/threads/thread.c
@@ -69,6 +69,7 @@ static void *alloc_frame(struct thread *, size_t size);
 static void schedule(void);
 void thread_schedule_tail(struct thread *prev);
 static tid_t allocate_tid(void);
+bool priority_less_func (const struct list_elem *a, const struct list_elem *b, void *aux);
 
 /*! Initializes the threading system by transforming the code
     that's currently running into a thread.  This can't work in
