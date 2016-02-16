e6ee17a89b552532466d8e8aba5994231647e15e
Jaden Geller <jaden.geller@gmail.com>
2016-02-15 22:33:40 -0800

    Built handler array and used macro to keep in sync with enum

--- src/lib/syscall-nr.h
+++ src/lib/syscall-nr.h
@@ -8,34 +8,37 @@
 #ifndef __LIB_SYSCALL_NR_H
 #define __LIB_SYSCALL_NR_H
 
+// Macro keeps enum and handler array in sync
+// http://rcr.io/words/syncing-enums-arrays.html
+#define SYSCALL_TYPES \
+    syscall_type(SYS_HALT,     sys_halt)     /*!< Halt the operating system. */             \
+    syscall_type(SYS_EXIT,     sys_exit)     /*!< Terminate this process. */                \
+    syscall_type(SYS_EXEC,     sys_exec)     /*!< Start another process. */                 \
+    syscall_type(SYS_WAIT,     sys_wait)     /*!< Wait for a child process to die. */       \
+    syscall_type(SYS_CREATE,   sys_create)   /*!< Create a file. */                         \
+    syscall_type(SYS_REMOVE,   sys_remove)   /*!< Delete a file. */                         \
+    syscall_type(SYS_OPEN,     sys_open)     /*!< Open a file. */                           \
+    syscall_type(SYS_FILESIZE, sys_filesize) /*!< Obtain a file's size. */                  \
+    syscall_type(SYS_READ,     sys_read)     /*!< Read from a file. */                      \
+    syscall_type(SYS_WRITE,    sys_write)    /*!< Write to a file. */                       \
+    syscall_type(SYS_SEEK,     sys_seek)     /*!< Change position in a file. */             \
+    syscall_type(SYS_TELL,     sys_tell)     /*!< Report current position in a file. */     \
+    syscall_type(SYS_CLOSE,    sys_close)    /*!< Close a file. */                          \
+                                                                                            \
+    /* Project 3 and optionally project 4. */                                               \
+    syscall_type(SYS_MMAP,     sys_mmap)     /*!< Map a file into memory. */                \
+    syscall_type(SYS_MUNMAP,   sys_munmap)   /*!< Remove a memory mapping. */               \
+                                                                                            \
+    /* Project 4 only. */                                                                   \
+    syscall_type(SYS_CHDIR,    sys_chdir)    /*!< Change the current directory. */          \
+    syscall_type(SYS_MKDIR,    sys_mkdir)    /*!< Create a directory. */                    \
+    syscall_type(SYS_READDIR,  sys_readdir)  /*!< Reads a directory entry. */               \
+    syscall_type(SYS_ISDIR,    sys_isdir)    /*!< Tests if a fd represents a directory. */  \
+    syscall_type(SYS_INUMBER,  sys_inumber)  /*!< Returns the inode number for a fd. */
+
 /*! System call numbers. */
-enum {
-    /* Projects 2 and later. */
-    SYS_HALT,                   /*!< Halt the operating system. */
-    SYS_EXIT,                   /*!< Terminate this process. */
-    SYS_EXEC,                   /*!< Start another process. */
-    SYS_WAIT,                   /*!< Wait for a child process to die. */
-    SYS_CREATE,                 /*!< Create a file. */
-    SYS_REMOVE,                 /*!< Delete a file. */
-    SYS_OPEN,                   /*!< Open a file. */
-    SYS_FILESIZE,               /*!< Obtain a file's size. */
-    SYS_READ,                   /*!< Read from a file. */
-    SYS_WRITE,                  /*!< Write to a file. */
-    SYS_SEEK,                   /*!< Change position in a file. */
-    SYS_TELL,                   /*!< Report current position in a file. */
-    SYS_CLOSE,                  /*!< Close a file. */
-
-    /* Project 3 and optionally project 4. */
-    SYS_MMAP,                   /*!< Map a file into memory. */
-    SYS_MUNMAP,                 /*!< Remove a memory mapping. */
-
-    /* Project 4 only. */
-    SYS_CHDIR,                  /*!< Change the current directory. */
-    SYS_MKDIR,                  /*!< Create a directory. */
-    SYS_READDIR,                /*!< Reads a directory entry. */
-    SYS_ISDIR,                  /*!< Tests if a fd represents a directory. */
-    SYS_INUMBER                 /*!< Returns the inode number for a fd. */
-};
+#define syscall_type(type, handler) type,
+enum { SYSCALL_TYPES };
+#undef syscall_type
 
 #endif /* lib/syscall-nr.h */
-
--- src/userprog/syscall.c
+++ src/userprog/syscall.c
@@ -6,12 +6,99 @@
 
 static void syscall_handler(struct intr_frame *);
 
+#define syscall_type(type, handler) void handler(struct intr_frame *f);
+SYSCALL_TYPES
+#undef syscall_type
+
+#define syscall_type(type, handler) handler,
+void (*handlers[])(struct intr_frame *) = { SYSCALL_TYPES };
+#undef syscall_type
+
 void syscall_init(void) {
     intr_register_int(0x30, 3, INTR_ON, syscall_handler, "syscall");
 }
 
-static void syscall_handler(struct intr_frame *f UNUSED) {
+void syscall_handler(struct intr_frame *f UNUSED) {
     printf("system call!\n");
     thread_exit();
 }
 
+void sys_halt(struct intr_frame *f UNUSED) {
+    
+}
+
+void sys_exit(struct intr_frame *f UNUSED) {
+    
+}
+
+void sys_exec(struct intr_frame *f UNUSED) {
+    
+}
+
+void sys_wait(struct intr_frame *f UNUSED) {
+    
+}
+
+void sys_create(struct intr_frame *f UNUSED) {
+    
+}
+
+void sys_remove(struct intr_frame *f UNUSED) {
+    
+}
+
+void sys_open(struct intr_frame *f UNUSED) {
+    
+}
+
+void sys_filesize(struct intr_frame *f UNUSED) {
+    
+}
+
+void sys_read(struct intr_frame *f UNUSED) {
+    
+}
+
+void sys_write(struct intr_frame *f UNUSED) {
+    
+}
+
+void sys_seek(struct intr_frame *f UNUSED) {
+    
+}
+
+void sys_tell(struct intr_frame *f UNUSED) {
+    
+}
+
+void sys_close(struct intr_frame *f UNUSED) {
+    
+}
+
+void sys_mmap(struct intr_frame *f UNUSED) {
+    
+}
+
+void sys_munmap(struct intr_frame *f UNUSED) {
+    
+}
+
+void sys_chdir(struct intr_frame *f UNUSED) {
+    
+}
+
+void sys_mkdir(struct intr_frame *f UNUSED) {
+    
+}
+
+void sys_readdir(struct intr_frame *f UNUSED) {
+    
+}
+
+void sys_isdir(struct intr_frame *f UNUSED) {
+    
+}
+
+void sys_inumber(struct intr_frame *f UNUSED) {
+    
+}
