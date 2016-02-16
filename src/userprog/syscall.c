866b1b69cc98aec07add46de616a950333ff8928
Jaden Geller <jaden.geller@gmail.com>
2016-02-15 22:37:12 -0800

    Added useful error messages for unimplemented functions

--- src/userprog/syscall.c
+++ src/userprog/syscall.c
@@ -24,81 +24,101 @@ void syscall_handler(struct intr_frame *f UNUSED) {
 }
 
 void sys_halt(struct intr_frame *f UNUSED) {
-    
+    printf("sys_halt!\n");
+    thread_exit();
 }
 
 void sys_exit(struct intr_frame *f UNUSED) {
-    
+    printf("sys_exit!\n");
+    thread_exit();
 }
 
 void sys_exec(struct intr_frame *f UNUSED) {
-    
+    printf("sys_exec!\n");
+    thread_exit();
 }
 
 void sys_wait(struct intr_frame *f UNUSED) {
-    
+    printf("sys_wait!\n");
+    thread_exit();
 }
 
 void sys_create(struct intr_frame *f UNUSED) {
-    
+    printf("sys_create!\n");
+    thread_exit();
 }
 
 void sys_remove(struct intr_frame *f UNUSED) {
-    
+    printf("sys_remove!\n");
+    thread_exit();
 }
 
 void sys_open(struct intr_frame *f UNUSED) {
-    
+    printf("sys_open!\n");
+    thread_exit();
 }
 
 void sys_filesize(struct intr_frame *f UNUSED) {
-    
+    printf("sys_filesize!\n");
+    thread_exit();
 }
 
 void sys_read(struct intr_frame *f UNUSED) {
-    
+    printf("sys_read!\n");
+    thread_exit();
 }
 
 void sys_write(struct intr_frame *f UNUSED) {
-    
+    printf("sys_write!\n");
+    thread_exit();
 }
 
 void sys_seek(struct intr_frame *f UNUSED) {
-    
+    printf("sys_seek!\n");
+    thread_exit();
 }
 
 void sys_tell(struct intr_frame *f UNUSED) {
-    
+    printf("sys_tell!\n");
+    thread_exit();
 }
 
 void sys_close(struct intr_frame *f UNUSED) {
-    
+    printf("sys_close!\n");
+    thread_exit();
 }
 
 void sys_mmap(struct intr_frame *f UNUSED) {
-    
+    printf("sys_mmap!\n");
+    thread_exit();
 }
 
 void sys_munmap(struct intr_frame *f UNUSED) {
-    
+    printf("sys_munmap!\n");
+    thread_exit();
 }
 
 void sys_chdir(struct intr_frame *f UNUSED) {
-    
+    printf("sys_chdir!\n");
+    thread_exit();
 }
 
 void sys_mkdir(struct intr_frame *f UNUSED) {
-    
+    printf("sys_mkdir!\n");
+    thread_exit();
 }
 
 void sys_readdir(struct intr_frame *f UNUSED) {
-    
+    printf("sys_readdir!\n");
+    thread_exit();
 }
 
 void sys_isdir(struct intr_frame *f UNUSED) {
-    
+    printf("sys_isdir!\n");
+    thread_exit();
 }
 
 void sys_inumber(struct intr_frame *f UNUSED) {
-    
+    printf("sys_inumber!\n");
+    thread_exit();
 }
