ffabd36820f672e560d7914b22b76ca7ac288d8d
Jaden Geller <jaden.geller@gmail.com>
2016-02-17 01:30:01 -0800

    Straightforward syscall arguments getting

--- src/userprog/syscall.c
+++ src/userprog/syscall.c
@@ -6,11 +6,8 @@
 
 static void syscall_handler(struct intr_frame *);
 
-#define ARG_N(f, type, n) (*(type *)((uint32_t *)((f)->esp) + (n)))
-#define ARG_0(f, type) ARG_N(f, type, 0)
-#define ARG_1(f, type) ARG_N(f, type, 1)
-#define ARG_2(f, type) ARG_N(f, type, 2)
-#define ARG_3(f, type) ARG_N(f, type, 3)
+#define GET_ARG(type, f, n) (*(type *)((uint32_t *)((f)->esp) + (n)))
+#define ARG(type, name, f, n) type name = GET_ARG(type, f, n)
 
 #define syscall_type(type, handler) void handler(struct intr_frame *f);
 SYSCALL_TYPES
@@ -26,7 +23,7 @@ void syscall_init(void) {
 
 void syscall_handler(struct intr_frame *f) {
     // Run system call
-    uint32_t syscall_id = ARG_0(f, uint32_t);
+    ARG(uint32_t, syscall_id, f, 0);
     handlers[syscall_id](f);
 }
 
@@ -36,97 +33,123 @@ void sys_halt(struct intr_frame *f UNUSED) {
 }
 
 void sys_exit(struct intr_frame *f UNUSED) {
-//    int status = ARG_0(
+    ARG(int, status UNUSED, f, 1);
     printf("sys_exit!\n");
     thread_exit();
 }
 
 void sys_exec(struct intr_frame *f UNUSED) {
+    ARG(const char *, file UNUSED, f, 1);
     printf("sys_exec!\n");
     thread_exit();
 }
 
 void sys_wait(struct intr_frame *f UNUSED) {
+    ARG(int, pid UNUSED, f, 1);
     printf("sys_wait!\n");
     thread_exit();
 }
 
 void sys_create(struct intr_frame *f UNUSED) {
+    ARG(const char *, file UNUSED, f, 1);
+    ARG(unsigned, initial_size UNUSED, f, 2);
     printf("sys_create!\n");
     thread_exit();
 }
 
 void sys_remove(struct intr_frame *f UNUSED) {
+    ARG(const char *, file UNUSED, f, 1);
     printf("sys_remove!\n");
     thread_exit();
 }
 
 void sys_open(struct intr_frame *f UNUSED) {
+    ARG(const char *, file UNUSED, f, 1);
     printf("sys_open!\n");
     thread_exit();
 }
 
 void sys_filesize(struct intr_frame *f UNUSED) {
+    ARG(int, fb UNUSED, f, 1);
     printf("sys_filesize!\n");
     thread_exit();
 }
 
 void sys_read(struct intr_frame *f UNUSED) {
+    ARG(int, fd UNUSED, f, 1);
+    ARG(void *, buffer UNUSED, f, 2);
+    ARG(unsigned, size UNUSED, f, 3);
     printf("sys_read!\n");
     thread_exit();
 }
 
 void sys_write(struct intr_frame *f UNUSED) {
+    ARG(int, fd UNUSED, f, 1);
+    ARG(const void *, buffer UNUSED, f, 2);
+    ARG(unsigned, size UNUSED, f, 3);
     printf("sys_write!\n");
     thread_exit();
 }
 
 void sys_seek(struct intr_frame *f UNUSED) {
+    ARG(int, fd UNUSED, f, 1);
+    ARG(unsigned, position UNUSED, f, 2);
     printf("sys_seek!\n");
     thread_exit();
 }
 
 void sys_tell(struct intr_frame *f UNUSED) {
+    ARG(int, fd UNUSED, f, 1);
     printf("sys_tell!\n");
     thread_exit();
 }
 
 void sys_close(struct intr_frame *f UNUSED) {
+    ARG(int, fd UNUSED, f, 1);
     printf("sys_close!\n");
     thread_exit();
 }
 
 void sys_mmap(struct intr_frame *f UNUSED) {
+    ARG(int, fd UNUSED, f, 1);
+    ARG(void *, addr UNUSED, f, 2);
     printf("sys_mmap!\n");
     thread_exit();
 }
 
 void sys_munmap(struct intr_frame *f UNUSED) {
+    ARG(int, mapid UNUSED, f, 1);
     printf("sys_munmap!\n");
     thread_exit();
 }
 
 void sys_chdir(struct intr_frame *f UNUSED) {
+    ARG(const char *, dir UNUSED, f, 1);
     printf("sys_chdir!\n");
     thread_exit();
 }
 
 void sys_mkdir(struct intr_frame *f UNUSED) {
+    ARG(const char *, dir UNUSED, f, 1);
     printf("sys_mkdir!\n");
     thread_exit();
 }
 
 void sys_readdir(struct intr_frame *f UNUSED) {
+    ARG(int, fd UNUSED, f, 1);
+    ARG(char *, name UNUSED, f, 2);
     printf("sys_readdir!\n");
     thread_exit();
 }
 
 void sys_isdir(struct intr_frame *f UNUSED) {
+    ARG(int, fd UNUSED, f, 1);
     printf("sys_isdir!\n");
     thread_exit();
 }
 
 void sys_inumber(struct intr_frame *f UNUSED) {
+    ARG(int, fd UNUSED, f, 1);
     printf("sys_inumber!\n");
     thread_exit();
 }
