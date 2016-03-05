601efb50178ba2d9ee3fc33b7fe50dbefa9caf2b
Jaden Geller <jaden.geller@gmail.com>
2016-03-05 04:18:52 -0800

    Added pinned property

--- src/vm/frame.h
+++ src/vm/frame.h
@@ -7,12 +7,13 @@
 
 struct frame_info {
     bool is_user_page;
-    
+    bool is_pinned;
     
     // TODO: Add pointer to supplementry page table entry
     //       so that we can update it when we evict it from
     //       physical memory.
     //   OR: Maybe just to the thread?
+    //    Q: What happens when the thread dies?
 };
 
 void frametable_init(void);
