fedbcdf8c9b67e60baae06d3f2a2fb4a95dfe6e0
Jaden Geller <jaden.geller@gmail.com>
2016-03-13 01:00:50 -0800

    Updated buffer to not block on acquire and to check and reacquire

--- src/filesys/buffer.c
+++ src/filesys/buffer.c
@@ -80,9 +80,9 @@ void buffer_init(void) {
 
 // This accepts a sector number and looks up the buffer_entry struct for that
 // sector. Returns null if the given sector isn't in the cache.
-struct buffer_entry *buffer_entry_for_sector(struct hash *buffer_table,
-                                             block_sector_t sector) {
-    
+// Precondition: Must hold the buffer_table_lock.
+struct buffer_entry *_buffer_entry_for_sector(block_sector_t sector) {
+
     ASSERT(lock_held_by_current_thread(&buffer_table_lock)); 
 
     // Create dummy entry for lookup
@@ -90,7 +90,7 @@ struct buffer_entry *buffer_entry_for_sector(struct hash *buffer_table,
     lookup_entry.occupied_by_sector = sector;
     
     // Get the buffer_entry associated with the given page.
-    struct hash_elem *e = hash_find(buffer_table, &lookup_entry.hash_elem);
+    struct hash_elem *e = hash_find(&buffer_table, &lookup_entry.hash_elem);
     if (e == NULL) {
         return NULL;
     } else {
@@ -98,89 +98,132 @@ struct buffer_entry *buffer_entry_for_sector(struct hash *buffer_table,
     }
 }
 
+// This accepts a sector number and looks up the buffer_entry struct for that
+// sector. Returns null if the given sector isn't in the cache.
+// Precondition: The global lock is held by the current thread.
+// Postcondition: The global lock is released only if a buffer entry is returned.
+struct buffer_entry *buffer_acquire_existing_entry(block_sector_t sector) {
+    ASSERT(lock_held_by_current_thread(&buffer_table_lock));
+    
+    // First grab the entry from the sector.
+    struct buffer_entry* b = _buffer_entry_for_sector(sector);
+    
+    // The buffer doesn't contain an entry for this sector.
+    // Return WITHOUT releasing the global lock!
+    if (b == NULL) return NULL;
+
+    // We found it, so let's avoid locking on the global lock.
+    // And make sure nobody is trying to steal our buffer entry!
+    lock_release(&buffer_table_lock);
+    lock_acquire(&b->lock);
+    
+    // Is this the same buffer entry we thought we retrieved?
+    while (b->occupied_by_sector != sector) {
+        // Nope, guess someone swapped it out. Rude.
+        
+        // Let go of this imposter and grab the global lock.
+        lock_release(&b->lock);
+        lock_acquire(&buffer_table_lock);
+        
+        // Let's try that again..
+        b = _buffer_entry_for_sector(sector);
+        
+        // We found it, so let's avoid locking on the global lock.
+        // And make sure nobody is trying to steal our buffer entry!
+        lock_release(&buffer_table_lock);
+        lock_acquire(&b->lock);
+    }
+    
+    // Guess we acquired it.
+    return b;
+}
+
 // Returns a pointer to an empty buffer slot.
 // If there isn't one, takes care of evicting one.
-// IMPORTANT: whatever slot is returned will have its lock acquired.
-// It is the callers responsibility to release that lock.
-struct buffer_entry* buffer_get_free_slot(void) {
+// Precondition: The global lock is held by the current thread.
+// Postcondition: The global lock is released, and
+//                the buffer entry's lock is acquired.
+struct buffer_entry* buffer_acquire_free_slot(void) {
 
 	// Any function that calls this function should have already
 	// acquired the lock on the buffer table.
 	ASSERT(lock_held_by_current_thread(&buffer_table_lock));
 
-	struct buffer_entry* b;
-
-	// First, let's just hope there's an unoccupied slot.
+    struct buffer_entry *b;
+	// Check if there's any unoccupied slots
 	if (buffer_unoccupied_slots > 0) {
 		// The first unoccupied slot we'll fill is index 63, the last is 0
 		buffer_unoccupied_slots -= 1;
-		lock_acquire(&(buffer[buffer_unoccupied_slots].lock));
-		b = &(buffer[buffer_unoccupied_slots]);
-		ASSERT(b->occupied_by_sector == UNOCCUPIED);
-		return b;
-	}
+        b = &(buffer[buffer_unoccupied_slots]);
+        lock_acquire(&b->lock);
+        ASSERT(b->occupied_by_sector == UNOCCUPIED);
 
-	// If there wasn't one, evict slot 1. We'll revisit
-	// this functionality later, obviously.
-	lock_acquire(&(buffer[1].lock));
-	b = &(buffer[1]);
-	hash_delete(&buffer_table, &(b->hash_elem));
-	b->occupied_by_sector = UNOCCUPIED;
-	return b;
+        // We can release the global lock after acquiring the buffer lock
+        // since we can't block---nobody to wait on for an unused buffer.
+        lock_release(&buffer_table_lock);
+	}
+    // Otherwise, evict an existing buffer entry...
+    else {
+        // Naively evict slot 1. We'll revisit
+        // this functionality later, obviously.
+        b = &buffer[1];
+        lock_acquire(&b->lock);
+        hash_delete(&buffer_table, &(b->hash_elem));
+        b->occupied_by_sector = UNOCCUPIED;
+
+        // TODO: Uncertain if we ought to block on the table lock in the
+        //       case of eviction. Reconsider later.
+        lock_release(&buffer_table_lock);
+    }
+    return b;
 }
 
 void buffer_read(block_sector_t sector, void* buffer) {
+    // Acquire the global lock. Will be automatically released
+    // when an existing entry or free slot is acquired.
+    lock_acquire(&buffer_table_lock);
+    struct buffer_entry* b;
+    
+    // First, let's see if sector is aleady in the buffer.
+    b = buffer_acquire_existing_entry(sector);
+    
+    // If this sector isn't yet loaded, load it here...
+    if (b == NULL) {
+        // Grab a buffer entry, lock acquired, and set it up.
+        b = buffer_acquire_free_slot();
+        b->occupied_by_sector = sector;
+        
+        // Read the on-disk data into the buffer.
+        block_read(fs_device, sector, &(b->storage));
+    }
+    
+    // Copy the data into the buffer
+    memcpy(buffer, &(b->storage), BLOCK_SECTOR_SIZE);
 
-	lock_acquire(&buffer_table_lock);
-	struct buffer_entry* b;
-
-	// First, let's see if sector is aleady in the buffer
-	b = buffer_entry_for_sector(&buffer_table, sector);
-
-	// If it was, fulfill the read from the buffer
-	if (b != NULL) {
-		lock_release(&buffer_table_lock);
-		lock_acquire(&(b->lock));
-		memcpy(buffer, &(b->storage), BLOCK_SECTOR_SIZE);
-		lock_release(&(b->lock));
-		return;
-	}
-
-	// If it wasn't, get a new cache slot and
-	// read from the disk into that slot.
-	b = buffer_get_free_slot();
-	lock_release(&buffer_table_lock);
-	block_read(fs_device, sector, &(b->storage));
-	memcpy(buffer, &(b->storage), BLOCK_SECTOR_SIZE);
-	b->occupied_by_sector = sector;
-	hash_insert(&buffer_table, &(b->hash_elem));
-	lock_release(&(b->lock));
+    // Release the lock on the buffer.
+    lock_release(&(b->lock));
 }
 
 void buffer_write(block_sector_t sector, const void* buffer) {
 
+    // Obtain the global lock to look up a buffer entry. Note that it
+    // will be released automatically if we find an entry, otherwise
+    // it must be released manually.
 	lock_acquire(&buffer_table_lock);
+    
 	struct buffer_entry* b;
+	// Try to find a cache entry.
+    if ((b = buffer_acquire_existing_entry(sector))) {
+        // Copy the buffer data into the cache.
+        memcpy(&(b->storage), buffer, BLOCK_SECTOR_SIZE);
 
-	// First, let's see if sector is already in the buffer
-	b = buffer_entry_for_sector(&buffer_table, sector);
-
-	// If it was, write to the cache then writeback immediately
-	if (b != NULL) {
-		lock_release(&buffer_table_lock);
-		lock_acquire(&(b->lock));
-		memcpy(&(b->storage), buffer, BLOCK_SECTOR_SIZE);
-		block_write(fs_device, sector, &(b->storage));
-		lock_release(&(b->lock));
-		return;
-	}
-
-	// If it wasn't, just write to disk.
-	lock_release(&buffer_table_lock);
-	block_write(fs_device, sector, buffer);
-
-	// lock_acquire(&(buffer[2].lock));
-	// memcpy(storage_of_index(2), buffer, BLOCK_SECTOR_SIZE);
-	// block_write(fs_device, sector, storage_of_index(2));
-	// lock_release(&(buffer[2].lock));
+        // We're done writing, so release the lock.
+        lock_release(&b->lock);
+    } else {
+        // Release the buffer lock.
+        lock_release(&buffer_table_lock);
+        
+        // Just write to disk.
+        block_write(fs_device, sector, buffer);
+    }
 }
